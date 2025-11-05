2d89b5a2cfe22d41c3bab898ec475a5e6af2fdc4
mdsharia4613 <mdsharia4613@gmail.com>
2025-11-05 18:08:46 +0600

    footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,42 +1,43 @@
-import React from 'react';
+import { FaFacebook, FaTwitter, FaYoutube } from 'react-icons/fa6';
 
 const Footer = () => {
     return (
         <div>
-            <footer className="footer sm:footer-horizontal bg-stone-800 p-10">
+            <footer className="footer sm:footer-horizontal bg-[#181818] p-10">
                 <nav>
-                    <h6 className="footer-title">Services</h6>
-                    <a className="link link-hover">Branding</a>
-                    <a className="link link-hover">Design</a>
-                    <a className="link link-hover">Marketing</a>
-                    <a className="link link-hover">Advertisement</a>
+                    <h6 className="footer-title">DRAMATIC</h6>
+                    <p>Enjoy your Movie time</p>
                 </nav>
                 <nav>
-                    <h6 className="footer-title">Company</h6>
-                    <a className="link link-hover">About us</a>
-                    <a className="link link-hover">Contact</a>
-                    <a className="link link-hover">Jobs</a>
-                    <a className="link link-hover">Press kit</a>
+                    <h6 className="footer-title">NAVIGATION</h6>
+                    <a className="link link-hover">HOME</a>
+                    <a className="link link-hover">TV SHOW</a>
+                    <a className="link link-hover">MOVIES</a>
+                    <a className="link link-hover">NEW</a>
                 </nav>
                 <nav>
                     <h6 className="footer-title">Legal</h6>
-                    <a className="link link-hover">Terms of use</a>
-                    <a className="link link-hover">Privacy policy</a>
-                    <a className="link link-hover">Cookie policy</a>
+                    <a className="link link-hover">Privacy Policy</a>
+                    <a className="link link-hover">Terms of Service</a>
+                    <a className="link link-hover">Cookie Preferemces</a>
+                    <a className="link link-hover">Corporate Information</a>
                 </nav>
-                <form>
-                    <h6 className="footer-title">Newsletter</h6>
-                    <fieldset className="w-80">
-                        <label>Enter your email address</label>
-                        <div className="join">
-                            <input
-                                type="text"
-                                placeholder="username@site.com"
-                                className="input input-bordered join-item" />
-                            <button className="btn btn-primary join-item">Subscribe</button>
-                        </div>
-                    </fieldset>
-                </form>
+                <nav>
+                    <h6 className="footer-title">TALK TO US</h6>
+                    <a className="link link-hover">support@ercom.com</a>
+                    <a className="link link-hover">+66 2399 1145</a>
+
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Folling US</h6>
+                    <div className='flex items-center gap-4'>
+                        <p><FaFacebook fontSize={20} /></p>
+                        <p><FaTwitter  fontSize={20}/></p>
+                        <p><FaYoutube  fontSize={20}/></p>
+                    </div>
+
+                </nav>
+
             </footer>
         </div>
     );
